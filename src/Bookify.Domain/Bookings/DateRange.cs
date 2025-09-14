b109de84f38597d904f437d3e89b1c0e7ae9f888
KhakimzhonovUB <khakimzhonovub@gmail.com>
2025-09-15 00:20:05 +0500

    feat: add booking entity with his value objects

---
+++ src/Bookify.Domain/Bookings/DateRange.cs
@@ -0,0 +1,19 @@
+﻿namespace Bookify.Domain.Bookings;
+
+public record DateRange
+{
+    public DateOnly Start { get; init; }
+    public DateOnly End { get; init; }
+    
+    private DateRange() { }
+
+    public static DateRange Create(DateOnly start, DateOnly end)
+    {
+        if (start > end)
+            throw new ApplicationException("End date precedes start date");
+
+        return new DateRange { Start = start, End = end };
+    }
+    
+    public int LengthInDays => End.DayNumber - Start.DayNumber;
+}
